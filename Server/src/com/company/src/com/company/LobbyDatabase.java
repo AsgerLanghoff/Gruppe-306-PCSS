f625012b466405473dafecbf027714e9abf8946c
signetoftgaard <56248226+signetoftgaard@users.noreply.github.com>
2020-10-27 14:39:50 +0100

    Merge branch 'master' into client

---
+++ Server/src/com/company/src/com/company/LobbyDatabase.java
@@ -0,0 +1,46 @@
+package src.com.company.src.com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LobbyDatabase {
+
+    String lobbyName;
+    String host;
+    List<String> players;
+    int ready = 0;
+
+    LobbyDatabase (String lobbyName, String host, List<String> players) {
+        this.host = host;
+        this.lobbyName = lobbyName;
+        this.players = players;
+    }
+
+    public void addPlayer(String player) {
+        this.players.add(player);
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public String getLobbyName() {
+        return lobbyName;
+    }
+
+    public int getReady() {
+        return ready;
+    }
+    
+    public void increaseReady() {
+        ready++;
+    }
+
+
+    public List<String> getPlayers() {
+        return players;
+    }
+
+
+
+}
