26a51506be4494f34db8c6d3833e3ab2ff164e06
Rasmus Schrøder <RasmusKS98@gmail.com>
2020-10-30 14:00:33 +0100

    Updated comments

--- FinalServer/src/com/company/Database.java
+++ FinalServer/src/com/company/Database.java
@@ -4,27 +4,27 @@ import java.util.*;
 
 public class Database {
 
-    private List<String>PlayerID = new ArrayList<>();
-    private List<LobbyDatabase> lobbies = new ArrayList<>();
+    private List<String>PlayerID = new ArrayList<>(); //Creates a ArrayList that contains the playerID's as strings
+    private List<LobbyDatabase> lobbies = new ArrayList<>(); //Creates a ArrayList that contains the lobbies from the LobbyDatabase
 
-    public List<String> getPlayerList() {
+    /*public List<String> getPlayerList() { //Is never used
         return this.PlayerID;
-    }
+    }*/
 
-    public void addPlayer(String player) {
+    public void addPlayer(String player) { //Adds the playerID to the ArrayList
         PlayerID.add(player);
     }
 
-    public void removePlayer(String player){
+    /*public void removePlayer(String player){ //Is never used
         PlayerID.remove(PlayerID.indexOf(player));
-    }
+    }*/
 
-    public void addLobby (String lobbyName, String host, List<String> players){
+    public void addLobby (String lobbyName, String host, List<String> players){ //Adds the lobbyname, host and the list of players to the lobbies ArrayList
         LobbyDatabase l = new LobbyDatabase(lobbyName,host,players);
         this.lobbies.add(l);
     }
 
-    public List<LobbyDatabase> getLobbies() {
+    public List<LobbyDatabase> getLobbies() { //
         return lobbies;
     }
 
--- FinalServer/src/com/company/LobbyDatabase.java
+++ FinalServer/src/com/company/LobbyDatabase.java
@@ -5,11 +5,15 @@ import java.util.List;
 
 public class LobbyDatabase {
 
+    //Variables
     String lobbyName;
     String host;
+
+    //List of players
     List<String> players;
     int ready = 0;
 
+    //LobbyDatabase constructor - assigns the host, lobby name and players to the variables
     LobbyDatabase (String lobbyName, String host, List<String> players) {
         this.host = host;
         this.lobbyName = lobbyName;
@@ -40,6 +44,6 @@ public class LobbyDatabase {
     public List<String> getPlayers() {
         return players;
     }
-    
+
 }
 
