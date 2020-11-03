49bf1a816b628083cc5336edf2d736aa5824342d
AsgerLanghoff <54887248+AsgerLanghoff@users.noreply.github.com>
2020-11-03 14:23:48 +0100

    final version of both server and lobby on the master

--- Tank game - Client/src/SubLobby.java
+++ Tank game - Client/src/SubLobby.java
@@ -1,42 +1,42 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class SubLobby {
+public class SubLobby { // the local subLobby class containing the lobbyName and the playerlist.
     private String lobbyName;
     private List<String> players = new ArrayList<String>();
     private String host;
 
 
-    SubLobby(String lobbyName, String host){
+    SubLobby(String lobbyName, String host){ // constructor
         this.lobbyName = lobbyName;
         this.host = host;
         addToPlayers(host);
     }
 
 
-    void addToPlayers(String p) {
+    void addToPlayers(String p) { // adding a player to the playerList
         this.players.add(p);
     }
 
-    public void setPlayers(List<String> players){
+    public void setPlayers(List<String> players){ // set the list of players to a specific list, for example one recieved from the server
         this.players = players;
     }
 
-    public String getLobbyName() {
+    public String getLobbyName() { // returning the lobbyname
         return lobbyName;
     }
 
-    public List<String> getPlayers() {
+    public List<String> getPlayers() { //returning the list of players
         return players;
     }
 
-    public void printPlayers(){
+    public void printPlayers(){ // printing the list of players in the console.
         for (int i = 0; i < players.size(); i++) {
             System.out.println(" - "+players.get(i));
         }
     }
 
-    public String getHost() {
+    public String getHost() {// returning the playerID of the host.
         return host;
     }
 }
